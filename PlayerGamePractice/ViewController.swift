47a39b2904a7a7af9c03e0c667edee3081d3c729
DennisAndreev <denisandreev1611@gmail.com>
2019-07-20 23:00:53 +0300

    Initial Commit

---
+++ PlayerGamePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerGamePractice
+//
+//  Created by Денис Андреев on 7/20/19.
+//  Copyright © 2019 Денис Андреев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
