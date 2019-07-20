6d4b29a3a36753685dade40f547a73ae5ee13825
DennisAndreev <denisandreev1611@gmail.com>
2019-07-20 23:24:03 +0300

    player

---
+++ PlayerGamePractice/GenreButton.swift
@@ -0,0 +1,30 @@
+//
+//  GenreButton.swift
+//  PlayerGamePractice
+//
+//  Created by Денис Андреев on 7/20/19.
+//  Copyright © 2019 Денис Андреев. All rights reserved.
+//
+
+import UIKit
+
+class GenreButton: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PlayerGamePractice/ViewController.swift
+++ PlayerGamePractice/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    var player = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func Tap1(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                 self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+    }
+    
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        player.stop()
+    }
+    
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+        player.skipToNextItem()
+    }
+    
+    func playGenre(genre:String){
+         player.stop()
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        query.addFilterPredicate(predicate)
+        player.setQueue(with: query)
+        player.shuffleMode = .songs
+        player.play()
+    }
 }
 
